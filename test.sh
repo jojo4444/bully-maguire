73ff8a8cca17392667f643744e38e3527060409a
giordano.ch <jojowork4444@gmail.com>
2022-12-25 18:36:18 +0200

    add bash-script test

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 goex() {
+	echo "panica!"
 	exit 126
 }
 
@@ -10,13 +11,27 @@ cmake .. -DCMAKE_BUILD_TYPE=Release || goex
 
 rm -f test ans || goex
 
-for i in $(seq 1 1)
+for i in $(seq 1 1000)
 do
 	echo "10 random" | ./generator/generator > test || goex
-	res=$(tail test -n 1)
-	cat test | ./solution/solution > ans
-	res2=$(tail ans -n 1)
+	res=($(tail test -n 1))
+	cat test | ./solution/solution > ans || goex
+	res2=($(tail ans -n 1))
 
-	echo $res
-	echo $res2
+	for i in $(seq 0 2)
+	do
+		a=${res[i]}
+		b=${res2[i]}
+
+		bcres=$(echo "tmp=($a)-($b);tmp > -0.0001 && tmp < 0.0001" | bc -l)
+
+		if [ "$bcres" != "1" ]
+		then
+			echo "Error!!!"
+			echo "bcres: $bcres"
+			echo "res: $res"
+			echo "res2: $res2"
+			goex
+		fi
+	done
 done
